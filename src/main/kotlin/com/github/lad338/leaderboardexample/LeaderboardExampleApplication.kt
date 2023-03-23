0bf4bcf381b2dbf6d4b7c263fcd68ddb825960d8
Dicky Li <dickyli338@gmail.com>
2023-03-23 14:39:02 -0400

    add spring init files

---
+++ src/main/kotlin/com/github/lad338/leaderboardexample/LeaderboardExampleApplication.kt
@@ -0,0 +1,11 @@
+package com.github.lad338.leaderboardexample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LeaderboardExampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<LeaderboardExampleApplication>(*args)
+}
