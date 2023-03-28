41f05dc14778aadd09a716b672484e9fbe0491a8
Dicky Li <dickyli338@gmail.com>
2023-03-28 02:50:04 -0400

    remove unused dependency in redis cache config

--- src/main/kotlin/com/github/lad338/leaderboardexample/config/RedisCacheConfig.kt
+++ src/main/kotlin/com/github/lad338/leaderboardexample/config/RedisCacheConfig.kt
@@ -11,14 +11,11 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.cache.RedisCacheConfiguration
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
 import org.springframework.data.redis.serializer.RedisSerializationContext
-import org.springframework.data.redis.serializer.RedisSerializer
 import java.time.Duration
 
 @Configuration
 @EnableCaching
-class RedisCacheConfig(
-    private val objectRedisSerializer: RedisSerializer<Any>
-) {
+class RedisCacheConfig {
     @Bean
     fun cacheConfiguration(): RedisCacheConfiguration? {
 
